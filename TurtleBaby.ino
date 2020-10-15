870aec33a2ce7a4d11d19a6bedffefc8210082b6
Chris Bradel <ChrisBradel14@gmail.com>
2020-10-15 16:03:47 -0400

    landscape

--- TurtleBaby.ino
+++ TurtleBaby.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduboy2.h>
+#include "spr_turtle.h"
 
-} 
+#define screen_width 128
+#define screen_height 64
 
-void loop() {
-  // put your main code here, to run repeatedly:
+#define horizon_height 32
 
+Arduboy2 arduboy;
+Sprites sprites;
+
+void setup()
+{
+  arduboy.begin();
+  arduboy.setFrameRate(10);
+}
+
+void loop()
+{
+  if (!arduboy.nextFrame())
+    return;
+
+  arduboy.clear();
+
+  // border
+  arduboy.drawRect(0, 0, screen_width, screen_height);
+
+  // horizon
+  arduboy.drawFastHLine(0, horizon_height, screen_width);
+
+  arduboy.display();
 }
